8e157b512a52f6d6fcfdcf96b1a64d0e24f31083
Mehmet Ali YILMAZ <mehmetaliyilmaz5371@gmail.com>
2022-06-23 12:23:18 +0300

    master

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import './Expenses.css';
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState();
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+
+  const filteredItems = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+
+        {
+          filteredItems.length === 0 ? <p>HELLOO BLYADDD</p> : filteredItems.map((expense) => (
+            <ExpenseItem
+              key={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+              date={expense.date}
+            />
+          ))
+        }
+
+
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
